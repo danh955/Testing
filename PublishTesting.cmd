25d8d2c28d222339c73954dd2f6b53aae19fb384
Dan Hilderbrand <16111574+danh955@users.noreply.github.com>
2022-03-14 14:42:02 +0900

    More generic file names.

--- PublishTesting.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-:: This build the project and put it in the danh955 NuGet repository.
-set versionSuffix=
-if not "%~1" == "" set versionSuffix=--version-suffix %1
-dotnet pack Testing --configuration Release %versionSuffix%
-dotnet nuget push Testing\bin\Release\*.nupkg --source "danh955" --skip-duplicate
